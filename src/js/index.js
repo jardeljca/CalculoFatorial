b833fc2d916e699d0c9c330950c2cf9a51bfbe00
Jardel Queiroz <jardeljcadm@gmail.com>
2024-11-28 01:12:23 -0300

    Inicio e conclusão do site em um só commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,14 @@
-function fatorial(numero) {
-    var numero = get.elementById("numero").value;
-    if (numero <= 0) {
-        alert("O número deve ser maior que 0");
-    } else {
-        while(numero > 0) {
-            var somaFatorial = numero * (numero - 1);
-            numero--;
-        }
+function calcularFatorial() {
+    const numero = document.getElementById('numero').value;
+    if (numero < 0) {
+        document.getElementById("num").innerText = "Fatorial não é definido para números negativos.";
+        return;
     }
+
+    let fatorial = 1;
+    for (let i = 1; i <= numero; i++) {
+        fatorial *= i;
+    }
+
+    document.getElementById("num").innerText = `O fatorial de (${numero}) = ${fatorial}.`;
 }
